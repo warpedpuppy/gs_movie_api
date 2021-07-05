b17d5dab8e409a9c71c7d19855d7840753ad35c9
gbstrom <gbstrom@gmail.com>
2021-07-05 13:57:08 -0400

    created initial version of server.js

--- server.js
+++ server.js
@@ -1,3 +1,41 @@
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
+
+http.createServer((request, response) => {
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Added to log.');
+      }
+    });
+
+
+    if (q.pathname.includes('documentation')) {
+      filePath = (__dirname + '/documentation.html');
+    } else {
+      filePath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data);
+      response.end();
+
+    });
+
+  }).listen(8080);
+  console.log('My test server is running on Port 8080.');
+/*
 const http = require('http');
 
 http.createServer((request, response) => {
@@ -6,3 +44,4 @@ http.createServer((request, response) => {
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
+*/
