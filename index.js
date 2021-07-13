dfc77df93006407446b81a677d6350bc47ff4ffe
gbstrom <gbstrom@gmail.com>
2021-07-13 00:31:46 -0400

    created provisional Express code routing endpoints described in the table in documentation.html

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
 const express = require('express'),
   morgan = require('morgan');
+  uuid = require('uuid');
+  bodyParser = require('body-parser');
 
 const app = express();
 
-let topTenMovies = [
-  {
-    title: 'The Long Goodbye',
-    director: 'Robert Altman'
-  },
-  {
-    title: 'A Serious Man',
-    director: 'Joel & Ethan Coen'
-  },
-  {
-    title: 'Tokyo Story',
-    director: 'Yasujiro Ozu'
-  },
-  {
-    title: 'The 49th Parallel',
-    director: 'Michael Powell'
-  },
-  {
-    title: 'The Hudsucker Proxy',
-    director: 'Joel & Ethcan Coen'
-  },
-  {
-    title: 'Blue Velvet',
-    director: 'David Lynch'
-  },
-  {
-    title: 'Secrets and Lies',
-    director: 'Mike Leigh'
-  },
-  {
-    title: 'Late Spring',
-    director: 'Yasujiro Ozu'
-  },
-  {
-    title: 'The Lady Vanishes',
-    director: 'Alfred Hitchcock'
-  },
-  {
-    title: 'After Hours',
-    director: 'Martin Scorsese'
-  }
-];
+let Users = [];
 
 app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie app!')
-});
+//return a list of ALL movies to the user
 
 app.get('/movies', (req, res) => {
-  res.json(topTenMovies);
+  res.send('successful GET request returning a list of all the movies');
+});
+
+//Return data (description, genre, director, image URL, whether it’s featured or
+//not) about a single movie by title to the user
+
+app.get('/movies/:title', (req, res) => {
+  res.send('successful GET request returning all the data about the requested movie');
+});
+
+//Return data about a genre (description) by name/title (e.g., “Thriller”)
+
+app.get('/genres/:genre', (req, res) => {
+  res.send('successful GET request returning a list of all the movies in the given genre');
+});
+
+//Return data about a director (bio, birth year, death year) by name
+
+app.get('/directors/:name', (req, res) => {
+  res.send('successful GET request returning all the data about the given director');
+});
+
+//Allow new users to register
+
+app.post('/users', (req, res) => {
+  res.send('successful POST request returning data about the new user and their new ID')
+});
+
+//Allow users to update their user info (username)
+
+app.put('/users/:username', (req, res) => {
+  res.send('successful PUT request updating user\'s username');
+});
+
+//Allow users to add a movie to their list of favorites (showing only a text
+//that a movie has been added—more on this later)
+
+app.post('/users/:username/favorites/:title', (req, res) => {
+  res.send('successful POST request adding movie to user\'s list of favorites');
+});
+
+//Allow users to remove a movie from their list of favorites (showing only a
+//text that a movie has been removed—more on this later)
+
+app.delete('/users/:username/favorites/:title', (req, res) => {
+  res.send('succesful DELETE request removing movie from user\'s list of favorites');
+});
+
+//Allow existing users to deregister (showing only a text that a user email has
+//been removed—more on this later)
+
+app.delete('/users/:username', (req, res) => {
+  res.send('succesful DELETE request removing user and user details from the app');
 });
 
 app.use(express.static('public'));
