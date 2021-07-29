48d75376016b748186b6ff12465622901156d9fe
gbstrom <gbstrom@gmail.com>
2021-07-29 00:32:27 -0400

    connected the code to atlas via environment variable protected in Heroku

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
   morgan = require('morgan');
